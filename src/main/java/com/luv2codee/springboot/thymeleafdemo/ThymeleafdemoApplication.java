f18f0c984560b9b12c7ef599a5eb5e900ff03227
Mohammed Salim Sulaiman Al-Salehi <4t.mohammed@TRA.COM>
2024-10-20 12:07:51 +0400

    This is a Spring MVC sample project

---
+++ src/main/java/com/luv2codee/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -0,0 +1,16 @@
+package com.luv2codee.springboot.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//This is a Spring MVC project
+
+@SpringBootApplication
+public class ThymeleafdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdemoApplication.class, args);
+
+	}
+
+}
